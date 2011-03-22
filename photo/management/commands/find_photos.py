0c63c9ed6b45f468471d941730988af862260326
Rasmus Kaj <kaj@kth.se>
2011-03-22 20:56:20 +0100

    Add a basic command for finding photos.

---
+++ photo/management/commands/find_photos.py
@@ -0,0 +1,43 @@
+from django.core.management.base import NoArgsCommand
+from photos.photo.models import *
+import os
+from os.path import join
+from PIL import Image
+from PIL.ExifTags import TAGS
+from datetime import datetime
+
+def decode_exif(filename):
+    i = Image.open(filename)
+    info = i._getexif()
+    ret = {}
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        ret[decoded] = value
+    return ret;
+
+class Command(NoArgsCommand):
+    help = 'Find and index photos.'
+    
+    def handle_noargs(self, **options):
+        for root, dirs, files in os.walk('/home/kaj/Bilder/foto/2010/02'):
+            #print "Current directory", root
+            #print "Sub directories", dirs
+            #print "Files", files
+            for fn in files:
+                filename = os.path.join(root, fn)
+                exif = decode_exif(filename)
+                date = datetime.strptime(exif['DateTimeOriginal'],
+                                         '%Y:%m:%d %H:%M:%S')
+                camera, is_new = Camera.objects.get_or_create(
+                    make = exif['Make'],
+                    model = exif['Model'])
+                print filename, date, camera
+                photo, is_new = Photo.objects.get_or_create(
+                    img = filename,
+                    defaults = { 'date': date, 'camera': camera })
+                if not is_new:
+                    photo.date = date;
+                    photo.camera = camera
+                    photo.save()
+                print photo, is_new
+
