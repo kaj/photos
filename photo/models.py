ed29e2f7a6d4eb8e8e7758acb8b65ec30f1d5163
Rasmus Kaj <kaj@kth.se>
2011-03-22 20:55:50 +0100

    Add __unicode__ methods for debugging.

--- photo/models.py
+++ photo/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from os.path import basename
 
 class Camera(models.Model):
     make = models.CharField(max_length=200)
     model = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return u'%s %s' % (self.make, self.model)
+
 class Photo(models.Model):
     img = models.ImageField(upload_to='TODO')
     date = models.DateTimeField('orignial date')
     camera = models.ForeignKey(Camera)
+
+    def __unicode__(self):
+        return u'%s (%s)' % (basename(self.img.url), self.date)
