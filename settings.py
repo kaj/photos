c0be6f89f4062dd9d4174dc4112e4a6285fe35eb
Rasmus Kaj <kaj@kth.se>
2011-04-14 21:10:10 +0200

    Enable the admin site.

--- settings.py
+++ settings.py
@@ -92,9 +92,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'sorl.thumbnail',
     
     'photo',
